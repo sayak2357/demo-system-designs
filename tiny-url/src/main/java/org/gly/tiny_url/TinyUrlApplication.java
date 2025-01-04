c5655605cdff95974b56a8ebd052c370af96df68
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2025-01-04 11:30:25 +0530

    tinyUrl initialized

---
+++ tiny-url/src/main/java/org/gly/tiny_url/TinyUrlApplication.java
@@ -0,0 +1,13 @@
+package org.gly.tiny_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlApplication.class, args);
+	}
+
+}
