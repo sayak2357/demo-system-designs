a54bc32195b828aee5c7261823c09200d6580a1a
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2025-09-24 19:39:18 +0530

    spelling correction

--- atm/src/Main.java
+++ atm/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
 
-        System.out.println("Hello and welcom to ATM servicee!");
+        System.out.println("Hello and welcome to ATM services!");
 
         // Using banking service
         User sam = new User("sam",1);
