d57b4e1c019717b258e9991b6ef921fb86b8e256
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2025-10-22 19:57:55 +0530

    fix

--- splitwise/src/main/java/com/splitwise/model/PercentageSplit.java
+++ splitwise/src/main/java/com/splitwise/model/PercentageSplit.java
@@ -7,7 +7,7 @@ public class PercentageSplit extends Split {
     private double percentage;
 
     public PercentageSplit(int paidUserId, double amount, List<Integer> beneficiaryIds,double percentage) {
-        super(paidUserId,beneficiaryIds,amount,SplitType.PERCENTGE);
+        super(paidUserId,beneficiaryIds,amount,SplitType.PERCENTAGE);
         this.percentage=percentage;
     }
 
